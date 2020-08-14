758bef56aeb0d31a70a03f68c2bfdbe2aff585de
Valentine Kiselev <mrexox@outlook.com>
2020-08-14 18:49:48 +0300

    Add comments, fix identation

--- clpm/cli.lisp
+++ clpm/cli.lisp
@@ -1,4 +1,4 @@
-;;; Command Line Interface
+;;;; Command Line Interface
 
 (defparameter *interpreter* '((:name . :sbcl))
   "Lisp interpreter settings")
@@ -8,12 +8,16 @@
 
 ;;; Parsing
 
+;;; Set interpreter requirements
+;;;
+;;; Example:
+;;;   (lisp :sbcl >= "1.2.3")
 (defmacro lisp (&rest definition)
   `(setf *interpreter* (remove-if #'(lambda (cell) (null (cdr cell)))
-                                      (list
-                                       (cons :name (first ',definition))
-                                       (cons :constraint (second ',definition))
-                                       (cons :version (third ',definition))))))
+                                  (list
+                                   (cons :name (first ',definition))
+                                   (cons :constraint (second ',definition))
+                                   (cons :version (third ',definition))))))
 
 (defun add-system-to-scope (&key scope name constraint version)
   (let* ((scope-metadata (gethash scope *scopes*))
@@ -28,15 +32,23 @@
     (when new-scopep
       (setf (gethash scope *scopes*) scope-metadata))))
 
+;;; Set dependencies by scope
+;;;
+;;; Example:
+;;;   (scope :development
+;;;     (:package-name >= "1.2.3"))
+;;;
+;;; Note:
+;;;   Use keywords for scope and package names
 (defmacro scope (key &rest systems)
   (let ((name (gensym))
         (constraint (gensym))
         (version (gensym)))
-  `(dolist (definition ',systems)
-    (let ((,name (first definition))
-          (,constraint (second definition))
-          (,version (third definition)))
-      (add-system-to-scope :scope ,key
-                            :name ,name
-                            :constraint ,constraint
-                            :version ,version)))))
+    `(dolist (definition ',systems)
+       (let ((,name (first definition))
+             (,constraint (second definition))
+             (,version (third definition)))
+         (add-system-to-scope :scope ,key
+                              :name ,name
+                              :constraint ,constraint
+                              :version ,version)))))
