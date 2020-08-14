3a4415a109efc86232ae6d164c93381fc72e1468
Valentine Kiselev <mrexox@outlook.com>
2020-08-14 10:08:04 +0300

    Refactor macroses for parsing systems.lisp

--- clpm/cli.lisp
+++ clpm/cli.lisp
@@ -1,44 +1,42 @@
 ;;; Command Line Interface
 
-(defparameter *lisp-interpreter* 'sbcl
-  "Lisp interpreter")
-(defparameter *lisp-interpreter-version* 0
-  "Lisp interpreter version")
-(defparameter *lisp-interpreter-version-restriction* '>=
-  "Lisp interpreter version restriction")
+(defparameter *interpreter* '((:name . :sbcl))
+  "Lisp interpreter settings")
 
 (defparameter *scopes* (make-hash-table)
   "The hash table of scoped dependencies")
 
-;;; Helper functions
+;;; Parsing
 
-(defun lisp (&rest body)
-  (assert (>= (length body) 2) ()
-          "Must specify at least lisp interpreter and it's version")
-  (let ((interpreter (first body))
-        (restriction (second body))
-        (version (third body)))
-    (format t "~A" interpreter)
-    (assert (eq interpreter 'sbcl) ()
-            "Currently only sbcl interpreter is supported")
-    (assert (if (null version) (find restriction '(> >= < <=)) t) ()
-            ("Lisp version must have > >= < <= restriction. >= is used if not given")))
-    (setq *lisp-interpreter* interpreter
-          *lisp-interpreter-version* (or version restriction)
-          *list-interpreter-version-restriction* (if (null version) '>= restriction)))
+(defmacro lisp (&rest definition)
+  `(setf *interpreter* (remove-if #'(lambda (cell) (null (cdr cell)))
+                                      (list
+                                       (cons :name (first ',definition))
+                                       (cons :constraint (second ',definition))
+                                       (cons :version (third ',definition))))))
 
 (defun add-system-to-scope (&key scope name constraint version)
-  (let ((scope-metadata (or (gethash scope *scopes*))))
-    ;; TODO
-    ))
-
+  (let* ((scope-metadata (gethash scope *scopes*))
+         (new-scopep (null scope-metadata))
+         (metadata (remove-if #'(lambda (cell) (null (cdr cell)))
+                              (list
+                               (cons :constraint constraint)
+                               (cons :version version)))))
+    (when new-scopep
+      (setf scope-metadata (make-hash-table)))
+    (setf (gethash name scope-metadata) metadata)
+    (when new-scopep
+      (setf (gethash scope *scopes*) scope-metadata))))
 
 (defmacro scope (key &rest systems)
-  (dolist (definition systems)
-    (let ((name (first definition))
-          (constraint (second definition))
-          (version (third definition)))
-      `(add-system-to-scope :scope key
-                            :name name
-                            :constraint constraint
-                            :version version))))
+  (let ((name (gensym))
+        (constraint (gensym))
+        (version (gensym)))
+  `(dolist (definition ',systems)
+    (let ((,name (first definition))
+          (,constraint (second definition))
+          (,version (third definition)))
+      (add-system-to-scope :scope ,key
+                            :name ,name
+                            :constraint ,constraint
+                            :version ,version)))))
